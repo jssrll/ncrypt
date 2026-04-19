80aadf1ba11a6d01ae4ecc06023c3fd2c2507b25
jssrll <jessrell1010@gmail.com>
2026-04-19 20:47:18 +0800

    Update

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 // Backend URL - HARDCODED (replace with your actual Apps Script URL)
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwuA5u6kyTtqcVhmVarGWvnalMLwYdArjvap9b6DY1ZAYYcFQqJbVRrw1j5NlwepGk/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwILCeJWcjkLbKjLryqBadpcqtHov1aRasrlP2aNUoAqOHcYN5PoaqkSAXJF2MnL_WZ/exec';
 
 // Current authenticated user
 let currentUser = null;
